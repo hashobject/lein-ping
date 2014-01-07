67168839831fd5382ab81401ccb61a8545f36edf
Anton Podviaznikov <podviaznikov@gmail.com>
2014-01-08 00:08:40 +0200

    update deps

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [overtone/at-at "0.0.1"]
-                 [clj-http "0.7.2"]]
+                 [overtone/at-at "1.2.0"]
+                 [clj-http "0.7.8"]]
   :eval-in-leiningen true)
